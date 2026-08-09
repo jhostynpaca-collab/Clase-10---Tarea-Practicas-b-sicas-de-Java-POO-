e224d5156f812f55daec9e3919f14d9eecb661c0
jhostynpaca-collab <jhostynpaca@gmail.com>
2026-08-08 20:19:53 -0500

    Constructor

--- Ejercicio1/src/Mascota.java
+++ Ejercicio1/src/Mascota.java
@@ -3,4 +3,9 @@ public class Mascota {
     private String nombre;
     private String especie;
     private int edad;
+
+public Mascota(String nombre, String especie, int edad) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
 }
